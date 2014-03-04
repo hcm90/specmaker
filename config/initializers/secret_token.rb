ddee12e7f1a179642d49baabdb8dc8c5a204833c
“hcm90” <heitor.moura@gmail.com>
2014-03-04 16:05:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Specmaker::Application.config.secret_key_base = 'fd223a1afdf8594b4b62802bb196f6f475e6327c41d10ee473fcc28eed29e6c20554ea22fa36de85fd57c29427fed7c50b7aaf68a6861f9b2ad140206b286dc6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Specmaker::Application.config.session_store :cookie_store, key: '_specmaker_session'
