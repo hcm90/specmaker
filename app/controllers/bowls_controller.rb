933cc926fb738378ed2cf65218e4b8b53992edec
Heather <helloheathergraham@gmail.com>
2014-03-04 17:01:53 -0500

    updated controllers with basic CRUD actions

--- app/controllers/bowls_controller.rb
+++ app/controllers/bowls_controller.rb
@@ -1,2 +1,39 @@
 class BowlsController < ApplicationController
+ def new
+ 	@bowl = Bowl.new
+ end
+
+ def create
+ 	@bowl = Bowl.new(bowl_params)
+ 	if @bowl.save
+			redirect_to bowl_url(@bowl) #WHERE SHOULD THIS REDIRECT?!
+		else
+			render :new
+		end
+	end
+ end
+
+ def edit
+		@bowl = Bowl.find(params[:id])
+	end
+
+	def update
+		@bowl = Bowl.find(params[:id])
+		if @bowl.update_attributes(bowl_params)
+			redirect_to bowl_path(@bowl)
+		else
+			render :edit
+		end
+	end
+
+	def delete
+		@bowl = Bowl.find(params[:id])
+		@bowl.destory
+		redirect_to bowls_path
+	end
+
+	private
+	def bowl_params
+		params.require(:bowl).permit(:left_right, :front_back, :depth, :bottom_grid, :colander, :specsheet_id)
+	end
 end
--- app/controllers/specsheets_controller.rb
+++ app/controllers/specsheets_controller.rb
@@ -1,2 +1,47 @@
 class SpecsheetsController < ApplicationController
+	def index
+		@specsheets = Specsheets.all
+	end
+
+	def show
+		@specsheet = Specsheet.find(params[:id])
+	end
+
+	def new
+		@specsheet = Specsheet.new
+	end
+
+	def create
+		@specsheet = Specsheet.new(specsheet_params)
+		if @specsheet.save
+			redirect_to specsheet_url(@specsheet)
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@specsheet = Specsheet.find(params[:id])
+	end
+
+	def update
+		@specsheet = Specsheet.find(params[:id])
+		if @specsheet.update_attributes(specsheet_params)
+			redirect_to specsheet_path(@specsheet)
+		else
+			render :edit
+		end
+	end
+
+	def delete
+		@specsheet = Specsheet.find(params[:id])
+		@specsheet.destory
+		redirect_to specsheets_path
+	end
+
+	private
+	def specsheet_params
+		params.require(:specsheet).permit(:name, :bowl, :installation, :corner_radius, :drain_location, :drain_size, :divider)
+	end
+
 end
